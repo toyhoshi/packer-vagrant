ae80f517f1e288d4ecf080e54de60326cf3ab557
toyhoshi <marco.varagnolo@gmail.com>
2019-08-30 16:03:45 +0200

    updated...

---
+++ scripts/base.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "debug: Executing scripts/base.sh"
+
+yum install -y epel-release
+
+yum -y update
+yum -y install gcc make gcc-c++ kernel-devel kernel-headers perl wget bzip2 tree man
+yum clean all
+
+echo "Restarting VM"
+shutdown -r now
